df197786407568090a3e3fe2d644a05a73db377b
furuhama <furuhama@users.noreply.github.com>
2020-06-09 22:41:01 +0900

    Update example for binary_search

--- examples/binary_search.rs
+++ examples/binary_search.rs
@@ -3,4 +3,7 @@ use competitive::binary_search::*;
 fn main() {
     let v = vec![0, 1, 2, 3, 3, 3, 4, 5, 6];
     assert_eq!(v.lower_bound(&3), 3);
+
+    let sqrt = binary_search(0.0, 100.0, |f| f * f < 256.0);
+    assert_eq!(sqrt, 16.0);
 }
